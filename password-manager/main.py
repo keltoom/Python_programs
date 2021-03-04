09504e7e311f4efa70cd047962a192ac5d785038
keltoom <77449245+keltoom@users.noreply.github.com>
2021-03-04 23:56:29 +0100

    exception handling  with json file

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,42 +1,61 @@
 from tkinter import *
 from tkinter import messagebox
-from random import choice,randint,shuffle
+from random import choice, randint, shuffle
 import pyperclip
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letters = [choice(letters) for char in range(randint(8, 10)) ]
-    password_symbols = [choice(symbols) for char in range(randint(2, 4)) ]
-    password_numbers = [choice(numbers) for char in range(randint(2, 4)) ]
-    password_list=password_letters+password_symbols+password_numbers
+    password_letters = [choice(letters) for char in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for char in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for char in range(randint(2, 4))]
+    password_list = password_letters + password_symbols + password_numbers
 
     shuffle(password_list)
-    password="".join(password_list)
-    password_input.insert(0,password)
+    password = "".join(password_list)
+    password_input.insert(0, password)
     pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_input.get()
     email_username = email_username_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "email": email_username,
+            "password": password
+        }
+    }
 
     if website == '' or password == '':
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"There are the details entered :\n"
-                                               f"Email: {email_username}\n"
-                                               f"Password: {password}\nIs it okay to save? ")
-
-        if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{website} | {email_username} | {password}\n")
+        # is_ok = messagebox.askokcancel(title=website,
+        #                                message=f"There are the details entered :\n"
+        #                                        f"Email: {email_username}\n"
+        #                                        f"Password: {password}\nIs it okay to save? ")
+        try:
+            with open("data.json", mode="r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", mode="w") as file:
+                json.dump(data, file, indent=4)
+        finally:
             reset()
 
 
@@ -75,7 +94,7 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 password_input = Entry(width=25)
 password_input.grid(column=1, row=3)
-generate_password_button = Button(text="Generate Password",command=generate_password)
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=30, command=save)
